626f3d7cc31d31ced9697bfdba799e69e3f40f8a
Kristin Anthony <anthkris@MacBook-Pro.local>
2021-05-01 01:21:55 -0700

    Update 404 page - Add better messaging and img to 404 page - Add link to 2019 version of portfolio

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,68 @@
 import React from 'react';
+import styled from 'styled-components';
+import SEO from '../components/SEO';
+import SadKrisBot from '../assets/images/krisBot_404.svg';
+
+const HalfPageStyles = styled.div`
+  display: flex;
+  gap: 2%;
+  align-items: center;
+  padding: 2rem;
+  .interior {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 2%;
+    align-items: center;
+  }
+  font-size: 1.2rem;
+
+  .fourOhFourContent {
+    @media (min-width: 650px) {
+      width: 58%;
+    }
+  }
+  .imageSection {
+    display: none;
+    justify-content: center;
+    margin: 0 auto;
+    @media (min-width: 650px) {
+      display: flex;
+      margin: initial;
+      justify-content: flex-end;
+      width: 38%;
+    }
+  }
+  .sadImg {
+    max-width: 300px;
+    width: 100%;
+    min-width: 200px;
+  }
+  .fourOhFourLink {
+    color: var(--link-pink);
+  }
+`;
 
 const FourOhFourPage = () => (
-  <div>
-    <p>Aww.... Page Not Found!</p>
-  </div>
+  <>
+    <SEO title="404 - Dead End" />
+    <HalfPageStyles className="interior">
+      <section className="imageSection">
+        <img className="sadImg" src={SadKrisBot} alt="KrisBot is sad" />
+      </section>
+      <section className="fourOhFourContent">
+        <h3>KrisBot regrets that you have hit a dead end.</h3>
+        <p>
+          Are you looking for my old site? Not to worry, I haven't banished it
+          from the internet. It just moved.
+        </p>
+        <p>
+          <a className="fourOhFourLink" href="https://2019.knanthony.com/">
+            2019 Portfolio Site
+          </a>
+        </p>
+      </section>
+    </HalfPageStyles>
+  </>
 );
 
 export default FourOhFourPage;
